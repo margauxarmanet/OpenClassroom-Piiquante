7a1ad5485c313ab978760ce790216385f9184a69
Margaux Armanet <112400605+margauxarmanet@users.noreply.github.com>
2023-04-03 17:34:49 +0200

    Étape 1
    Démarrer le serveur backend

---
+++ config/connectDB.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+  } catch (err) {
+    console.error(
+      `Failed to connect to database: ${err.message}. Exiting application.`
+    );
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const normalizePort = require('./utils/normalizePort');
+
+const mongoose = require('mongoose');
+const connectDB = require('./config/connectDB');
+const { requestLogger, errorLogger } = require('./middleware/logEvents');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const errorHandler = require('./middleware/errorHandler');
+
+const PORT = normalizePort(process.env.PORT || '3000');
+app.set('PORT', PORT);
+
+// Connect to MongoDB
+connectDB();
+
+// Monitoring requests
+app.use(requestLogger);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// Parse req.body in JSON format
+app.use(express.json());
+
+// Monitoring errors
+app.use(errorLogger);
+
+app.on('error', errorHandler);
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
